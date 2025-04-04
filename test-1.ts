55a127adafe2f72f3d9cd37d20c1c8b786c23789
pnkwa <pnkwa@192.168.1.7>
2025-04-04 07:17:08 +0700

    clean code

--- test-1.ts
+++ test-1.ts
@@ -1,17 +1,17 @@
 function customSort(arr: number[]): number[] {
-  const freqMap = new Map<number, number>();
+  const freqMap = new Map<number, number>()
 
-  arr.forEach((num) => freqMap.set(num, (freqMap.get(num) || 0) + 1));
+  arr.forEach((num) => freqMap.set(num, (freqMap.get(num) || 0) + 1))
 
   return arr.sort((a, b) => {
-    const freqA = freqMap.get(a)!;
-    const freqB = freqMap.get(b)!;
+    const freqA = freqMap.get(a)!
+    const freqB = freqMap.get(b)!
 
-    if (freqA === freqB) return a - b;
+    if (freqA === freqB) return a - b
 
-    return freqA - freqB;
-  });
+    return freqA - freqB
+  })
 }
 
-const input = [2, 3, 4, 4, 34, 6, 7, 2, 3, 7, 8, 8, 8, 7, 9, 10, 41, 8];
-console.log(customSort(input));
+const input = [2, 3, 4, 4, 34, 6, 7, 2, 3, 7, 8, 8, 8, 7, 9, 10, 41, 8]
+console.log(customSort(input))
